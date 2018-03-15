a488b8c401f1065bfd4d1577ce6029e452ba7ee4
Richie <isuperqiang@gmail.com>
2018-03-15 08:58:34 +0800

    command pattern

---
+++ src/com/richie/learning/design_pattern/command/ICommand.java
@@ -0,0 +1,12 @@
+package com.richie.learning.design_pattern.command;
+
+/**
+ * @author Richie on 2018.03.15
+ * 电视机控制命令，抽象命令角色
+ */
+public interface ICommand {
+    /**
+     * 执行命令
+     */
+    void execute();
+}
