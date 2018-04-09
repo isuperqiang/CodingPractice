099530851305d24a4ac55d7b97b5070116b769df
Richie <isuperqiang@gmail.com>
2018-04-10 07:36:04 +0800

    strategy pattern

---
+++ src/com/richie/learning/design_pattern/strategy/OutContext.java
@@ -0,0 +1,20 @@
+package com.richie.learning.design_pattern.strategy;
+
+/**
+ * @author Richie on 2018.04.10
+ */
+public class OutContext {
+    private IOutWay outWay;
+
+    public OutContext(IOutWay outWay) {
+        this.outWay = outWay;
+    }
+
+    public void setOutWay(IOutWay outWay) {
+        this.outWay = outWay;
+    }
+
+    public void go2Work() {
+        outWay.goOut();
+    }
+}
